cc18db81c1cda2fb06e751ad5feb2ebce9309a72
SergeChurkin-ca <sergechurkin@yahoo.com>
2021-02-26 10:20:59 -0500

    establsiheing server, set router, html

--- 1-node-farm/index.js
+++ 1-node-farm/index.js
@@ -1,41 +1,64 @@
 const fs = require('fs')
 const http = require('http');
+const url = require('url');
 
 
-/////////////////////////////////
-// FILES
+const replaceTemplate = (temp, product) => {
+    let output = temp.replace(/{%PRODUCTNAME%}/g, product.productName);
+    output = output.replace(/{%IMAGE%}/g, product.image);
+    output = output.replace(/{%PRICE%}/g, product.price);
+    output = output.replace(/{%NUTRIENTS%}/g, product.nutrients);
+    output = output.replace(/{%QUANTITY%}/g, product.quantity);
+    output = output.replace(/{%DESCRIPTIONT%}/g, product.description);
+    output = output.replace(/{%IMAGE%}/g, product.image);
 
-// Blocking, synchronous way
-// const textIn = fs.readFileSync('./starter/txt/input.txt', 'utf-8')
-// console.log(textIn)
-// const textOut = `this is hwat we know about the avocado: ${textIn}.\n Created on ${Date.now()}`
-// fs.writeFileSync('./starter/txt/output.txt', textOut)
-// console.log('File written')
+    if (!product.organic) output = output.replace(/{%NOT_ORGANIC%}/g, 'not-organic');
+    return output;
+}
 
-//  Non-blokcing, asynchronous way
+const tempOverview = fs.readFileSync(`${__dirname}/starter/templates/template-overview.html`, 'utf-8');
+const tempCard = fs.readFileSync(`${__dirname}/starter/templates/template-card.html`, 'utf-8');
+const tempProduct = fs.readFileSync(`${__dirname}/starter/templates/template-product.html`, 'utf-8');
 
-// fs.readFile('./starter/txt/start.txt', 'utf-8', (err, data1) => {
-//     return err ? console.log('error') :
-//         fs.readFile(`./starter/txt/${data1}.txt`, 'utf-8', (err, data2) => {
-//             console.log(data2);
-//             fs.readFile('./starter/txt/append.txt', 'utf-8', (err, data3) => {
-//                 console.log(data3);
-
-//                 fs.writeFile('./starter/txt/final.txt', `${data2}\n${data3}`, 'utf-8', (err) => {
-//                     console.log('Your file has been written')
-//                 })
-//             })
-//         })
-// })
-
-// console.log('Will read file')
+const data = fs.readFileSync(`${__dirname}/starter/dev-data/data.json`, 'utf-8');
+const dataObj = JSON.parse(data);
 
 /////////////////////////////////
 // SERVER
 const server = http.createServer((req, res) => {
-    res.end('Hello from the server')
+    const pathName = req.url
+
+    //overview page
+    if (pathName === '/' || pathName === '/overview') {
+        res.writeHead(200, { 'Content-type': 'text/html' })
+
+
+        const cardsHtml = dataObj.map((el) => replaceTemplate(tempCard, el)).join('')
+        const output = tempOverview.replace('{%PRODUCT_CARDS%}', cardsHtml)
+
+        res.end(output)
+
+        // product page
+    } else if (pathName === '/product') {
+        res.end('this is the PRODUCT 🌮')
+
+        // api page
+    } else if (pathName === '/api') {
+
+        res.writeHead('200', { 'Content-type': 'application/json' })
+        res.end(data)
+
+        // 404 page
+    } else {
+        res.writeHead(404, {
+            'Content-type': 'text/html',
+            'my-own-header': 'hello test'
+        })
+        res.end(`<h1> Page not found! </h1>`)
+    }
+    res.end('Hello from the server🤑')
 })
 
 server.listen(8000, '127.0.0.1', () => {
-    console.log('Listening to request on port 8000')
+    console.log('Listening to request on port 8000 🤘')
 })
